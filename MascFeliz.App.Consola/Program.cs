b56c84dbbd0c7fa0f470eea1787c367064ad78f3
Jaime Tovar <jtovar1902@gmail.com>
2021-10-03 17:10:49 -0500

    añado consola

---
+++ MascFeliz.App.Consola/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using MascFeliz.App.Dominio;
+using MascFeliz.App.Persistencia;
+
+
+
+
+
+namespace MascFeliz.App.Consola
+{
+    class Program
+    {
+        private static IRepositorioMascota _repoMascota = new RepositorioMascota(new Persistencia.AppContext());
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            AddMascota();
+           // BuscarMascota(1);
+            
+        }
+
+        private static void AddMascota()
+        {
+            var mascota = new Mascota {
+
+                Nombre="Rex",
+                Edad="5 años",
+                Peso="15kg",
+             //  Propietario=new Propietario();
+                Tipo="Canino",
+                Temperatura="36 ",
+                Frec_Cardiaca="90 ",
+                Frec_Respiratoria="40 ",
+                Recomendacion="Desparasitar"
+          
+            };
+             object p = _repoMascota.AddMascota(mascota);
+
+
+
+        }
+
+        //private static void BuscarMascota(int idMascota)
+       // {
+
+         //   var mascota = _repoMascota.GetMascota(idMascota);
+         //   Console.WriteLine(mascota.Nombre+" "+mascota.Edad);
+
+       // }
+    }
+}
